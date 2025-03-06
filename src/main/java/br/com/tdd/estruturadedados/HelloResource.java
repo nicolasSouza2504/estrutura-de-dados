59bccbb5f13bf845735d194d3541d9f793f21223
nicolas <nicolas.souza@moinformatica.com>
2025-03-05 21:31:03 -0300

    First commit

---
+++ src/main/java/br/com/tdd/estruturadedados/HelloResource.java
@@ -0,0 +1,14 @@
+package br.com.tdd.estruturadedados;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
