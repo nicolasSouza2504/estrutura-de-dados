0a56b667edac55cec6e5f4c7b3b4b4f9e3cca25f
nicolas <nicolas.souza@moinformatica.com>
2025-03-05 21:32:38 -0300

    Commit

--- src/main/java/br/com/tdd/estruturadedados/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package br.com.tdd.estruturadedados;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
